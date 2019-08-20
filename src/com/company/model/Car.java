df7fd4f9aca67d86abf17073369dbe85e5017436
kihors <kihors@gmail.com>
2019-08-20 21:44:35 +0300

    added model

---
+++ src/com/company/model/Car.java
@@ -0,0 +1,35 @@
+package com.company.model;
+
+public class Car {
+    private String name;
+    private int power;
+
+    public Car(String name, int power) {
+        this.name = name;
+        this.power = power;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public void setPower(int power) {
+        this.power = power;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "name='" + name + '\'' +
+                ", power=" + power +
+                '}';
+    }
+}
